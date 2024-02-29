2be3e6bd2c2760ec1f4b4286e3e71ed68560a3b8
Marco Pérez <marcodamianperez@gmail.com>
2024-02-29 14:28:12 -0300

    add get routes

--- api/app.js
+++ api/app.js
@@ -1,6 +1,49 @@
 import express from "express";
+import { readFileSync } from "node:fs";
 
 const app = express();
+
+const users = JSON.parse(
+  readFileSync("./data/users.json", { encoding: "utf-8" })
+);
+
+app.get("/", (req, res) => {
+  res.redirect("/api/users");
+});
+
+app.get("/api", (req, res) => {
+  res.redirect("/api/users");
+});
+
+// get all users
+app.get("/api/users", (req, res) => {
+  try {
+    res.status(200).send({
+      count: users.length,
+      users: users,
+    });
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+});
+
+// get a single user
+app.get("/api/users/:id", (req, res) => {
+  try {
+    const userId = req.params.id;
+    const user = users.find((user) => user.id == userId);
+    userId <= 0 || userId > 50 || isNaN(userId)
+      ? res.status(404).send({ message: "User not found" })
+      : res.status(200).send(user);
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+});
+
+// other routes
+app.get("*", (req, res) => {
+  res.status(404).send({ message: "Resource not found" });
+});
 
 app.listen(3000, () => {
   console.log("Server listening and running on port 3000");
